1d3367a0c0d968e8e3de88e79ba8a38c48ee7ea1
doublemarket <doublemarket@gmail.com>
2019-02-27 23:43:55 +0900

    initial commit

---
+++ js/background.js
@@ -0,0 +1,49 @@
+const itemBaseUrl = "https://news.ycombinator.com/item?id=";
+const userBaseUrl = "https://news.ycombinator.com/user?id=";
+const apiBaseUrl = "http://hn.algolia.com/api/v1/search_by_date?restrictSearchableAttributes=url&hitsPerPage=50&query=";
+const cacheTtl = 3600*12;
+
+function setBadgeUsingCache(url) {
+  const queryUrl = encodeURIComponent(url.replace(/^https?:\/\//, ""));
+  chrome.storage.local.get(queryUrl, function(r) {
+    var tsNow = Math.round((new Date()).getTime() / 1000);
+    var result = r[queryUrl]
+    if (Object.keys(r).length > 0 && result.split(":")[1] > (tsNow - cacheTtl)) {
+      var points = result.split(":")[0];
+      chrome.browserAction.setBadgeText({text:String(points)});
+    } else {
+      fetch(apiBaseUrl+queryUrl).then(function(response) {
+        if (!response.ok) {
+          throw new Error();
+        };
+        return response.json();
+      }).then(function(json) {
+        var points;
+        if (json.nbHits > 0) {
+          points = json.hits[0].points;
+        } else {
+          points = 0;
+        }
+        chrome.browserAction.setBadgeText({text:String(points)});
+        chrome.storage.local.set({[queryUrl]: points+":"+tsNow}, function() {
+        });
+      }).catch(function(error) {
+        console.log(error);
+      });
+    }
+  });
+}
+
+chrome.browserAction.setBadgeBackgroundColor({color:"#ff742b"});
+
+chrome.tabs.onActivated.addListener(function(tabId, windowId) {
+  chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) {
+    setBadgeUsingCache(tabs[0].url);
+  });
+});
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  if (changeInfo.url !== undefined && tab.active) {
+    setBadgeUsingCache(tab.url);
+  }
+});
---
+++ js/popup.js
@@ -0,0 +1,68 @@
+const itemBaseUrl = "https://news.ycombinator.com/item?id=";
+const userBaseUrl = "https://news.ycombinator.com/user?id=";
+const searchApiBaseUrl = "http://hn.algolia.com/api/v1/search?restrictSearchableAttributes=url&hitsPerPage=50&query=";
+const searchBaseUrl = "http://hn.algolia.com/?restrictSearchableAttributes=url&hitsPerPage=50&query=";
+const hnApiBaseUrl = "https://api.hackerwebapp.com/item/";
+const submitBaseUrl = "https://news.ycombinator.com/submitlink?u=";
+
+function renderComment(comments) {
+  var body = "";
+  for (let comment of comments) {
+    body += "<div class='comment'>";
+    body += "<p class='small'><a href='"+userBaseUrl+comment.user+"' target='_blank'>"+comment.user+"</a> "+comment.time_ago+"</p>";
+    body += "<div class='comment-body'>"+comment.content+"</div>";
+    if (comment.comments.length > 0) {
+      body += "<div class='comment-child'>";
+      body += renderComment(comment.comments);
+      body += "</div>";
+    }
+    body += "</div>";
+  }
+  return body;
+}
+
+$(function () {
+  chrome.tabs.getSelected(null, function(tab) {
+    const queryUrl = encodeURIComponent(tab.url.replace(/^https?:\/\//, ""));
+    fetch(searchApiBaseUrl+queryUrl).then(function(searchResponse) {
+      if (!searchResponse.ok) {
+        throw new Error();
+      };
+      return searchResponse.json();
+    }).then(function(json) {
+      if (json.nbHits > 0) {
+        var tsNow = Math.round((new Date()).getTime() / 1000);
+        var hits = json.nbHits;
+        var points = json.hits[0].points;
+        var itemUrl = itemBaseUrl+json.hits[0].objectID;
+        var authorName = json.hits[0].author;
+        var numComments = json.hits[0].num_comments;
+        chrome.browserAction.setBadgeText({text:String(points)});
+        chrome.storage.local.set({[queryUrl]: points+":"+tsNow}, function() {});
+        $('#search').html("<a href='"+searchBaseUrl+queryUrl+"' target='_blank'>"+hits + " item(s) found</a>");
+        $('#title').html("<a href='"+itemUrl+"' target='_blank'>"+json.hits[0].title+"</a>");
+        $('#url').html("<a href='"+json.hits[0].url+"' target='_blank'>"+json.hits[0].url+"</a>");
+        $('#item-info').html(points+" points by <a href='"+userBaseUrl+authorName+"' target='_blank'>"+authorName+"</a> "+timeago.format(json.hits[0].created_at)+" | "+numComments+" comments");
+
+        fetch(hnApiBaseUrl+json.hits[0].objectID).then(function(response) {
+          return response.json();
+        }).then(function(itemJson) {
+          if (itemJson.comments.length > 0) {
+            $('.body').html(renderComment(itemJson.comments));
+          } else {
+            $('.body').text("No comments added.");
+          }
+        });
+      } else {
+        chrome.browserAction.setBadgeText({text:String(0)});
+        chrome.storage.local.set({[queryUrl]: points+":"+tsNow}, function() {});
+        $('#search').text("0 item found");
+        var msg = "No item found for this page.";
+        msg += "<p><a href='"+submitBaseUrl+tab.url+"&t="+tab.title+"' target='_blank'>Submit this page to Hacker News.</a></p>";
+        $('.body').html(msg);
+      }
+    }).catch(function(error) {
+      $('.body').text("Failed to get data.");
+    });
+  });
+});
